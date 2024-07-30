77097958fa164a856e3db2f0cb28f4b9eb67f892
Ivy Xu <140064242+ivymxu@users.noreply.github.com>
2024-07-30 03:59:10 +0000

    function to get emissions information of food item

--- app integration/emissions_tracker.py
+++ app integration/emissions_tracker.py
@@ -11,3 +11,12 @@ data = pd.read_csv('../data/food_emissions.csv')
 # Endpoint to get emissions data
 @app.route('/get_emissions', methods=['POST'])
 
+def get_emissions():
+    # Get the food item from the request
+    food_item = request.json['food_item']
+    
+    # Get the emissions for the food item
+    emissions = data[data['food_item'] == food_item]['emissions'].values[0]
+    
+    # Return the emissions
+    return jsonify({'emissions': emissions})
