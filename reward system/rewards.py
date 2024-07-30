f0af4b7b80622de9229176490df90a4e48c3896a
Ivy Xu <140064242+ivymxu@users.noreply.github.com>
2024-07-30 04:03:38 +0000

    created class for rewards system

---
+++ reward system/rewards.py
@@ -0,0 +1,14 @@
+class RewardsSystem:
+    def __init__(self):
+        self.rewards = 0
+    
+    def calculate_points(self, emissions):
+        # if statements for better food choices
+
+        return self.rewards
+
+    def get_reward(self):
+        # determine type of reward based on points
+
+        return self.rewards
+
