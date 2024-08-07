9dc2d411ec552a61ffeb53e791d97ab595db5371
Ivy Xu <140064242+ivymxu@users.noreply.github.com>
2024-08-07 00:02:18 +0000

    print fun fact before converting to a function

--- reward system/fun_facts.py
+++ reward system/fun_facts.py
@@ -2,3 +2,8 @@ import requests
 
 limit = 1
 api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
+response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error: ", response.status_code, response.text)
